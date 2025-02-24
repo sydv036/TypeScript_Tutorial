ea03c8aa1180c8508353e317b43c6c1572c866fc
MSI\Admin <101080058+sydvph19885@users.noreply.github.com>
2025-02-24 22:32:34 +0700

    write Hello word and add type

---
+++ src/hoidanit-typescript-basic-master/hoidanit-typescript-basic-master/projects/lesson4/lesson4.js
@@ -0,0 +1,14 @@
+const sum1 = (x, y) => {
+  return x + y;
+};
+console.log("sum1", sum1(1, 5));
+
+const sum2 = (x, y) => {
+  return x + y;
+};
+console.log("checkSum2", sum2("name", 10));
+
+const sum3 = (x: number, y: number) => {
+  return x + y;
+};
+console.log("checkSum2", sum2("name", 10));
---
+++ src/hoidanit-typescript-basic-master/hoidanit-typescript-basic-master/projects/lesson4/lesson4.ts
@@ -0,0 +1,4 @@
+const sum3 = (x: number, y: number) => {
+  return x + y;
+};
+console.log("checkSum2", sum3(10, 10));
